c0c7562227a399ed6abc46d1f8d3f901c74320a8
binmex <robin019@hotmail.com>
2024-03-09 12:00:44 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Test from './components/Test'
+
+function App() {
+  return (
+    <>
+      <p>Hola</p>
+      <Test></Test>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import "primereact/resources/themes/bootstrap4-dark-blue/theme.css"; //estilos
+import 'primeicons/primeicons.css'; //iconos
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+)
