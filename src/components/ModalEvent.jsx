12a591cb7a814341d43630f5fc66977d2d6736bc
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2024-03-09 19:04:19 -0500

    Barra de busqueda

---
+++ src/components/ModalEvent.jsx
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import { Button } from "primereact/button";
+import { Dialog } from "primereact/dialog";
+import { InputNumber } from "primereact/inputnumber";
+import { InputText } from "primereact/inputtext";
+import { Calendar } from "primereact/calendar"; // Importar el componente Calendar
+
+const ModalEvent = () => {
+  const [visible, setVisible] = useState(false);
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [date, setDate] = useState(null); // Estado para la fecha, inicializado como null
+
+  const headerElement = (
+    <div className="inline-flex align-items-center justify-content-center gap-2">
+      <span className="font-bold white-space-nowrap">Agregar Disciplina</span>
+    </div>
+  );
+
+  const clean = () => {
+    setId("");
+    setName("");
+    setDate(null);
+  };
+
+  const saveEvent = () => {
+    // Datos del evento que deseas enviar
+    const eventData = {
+      id: id, // Asegúrate de tener las variables id, name y date definidas
+      name: name,
+      date: date,
+    };
+
+    // Configuración de la solicitud
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json", // Especifica el tipo de contenido del cuerpo de la solicitud
+      },
+      body: JSON.stringify(eventData), // Convierte el objeto eventData a formato JSON
+    };
+
+    // URL de la API
+    const url = "http://localhost:4000/event"; // Reemplaza con la URL de tu API
+
+    // Realizar la solicitud POST
+    fetch(url, requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error al guardar el evento");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Manejar la respuesta si es necesario
+        console.log("Evento guardado correctamente:", data);
+      })
+      .catch((error) => {
+        console.error("Error al guardar el evento:", error);
+      });
+  };
+
+  const footerContent = (
+    <div>
+      <Button
+        label="limpiar"
+        icon="pi pi-eraser"
+        severity="warning"
+        onClick={clean}
+      />
+      <Button label="Guardar" icon="pi pi-user-edit" severity="success" />
+    </div>
+  );
+
+  return (
+    <div
+      className="card flex justify-content-center"
+      style={{ paddingLeft: "5px" }}
+    >
+      <Button icon="pi pi-save" onClick={() => setVisible(true)} />
+      <Dialog
+        visible={visible}
+        modal
+        header={headerElement}
+        footer={footerContent}
+        style={{ width: "50rem" }}
+        onHide={() => setVisible(false)}
+      >
+        <div className="card flex flex-column md:flex-row gap-3">
+          <div className="p-inputgroup flex-1">
+            <span className="p-inputgroup-addon">ID</span>
+            <InputText
+              placeholder="ID"
+              value={id} // Asegúrate de enlazar el valor del input con el estado
+              onChange={(e) => {
+                const value = e.target.value;
+                const regex = /^[0-9]*$/; // Expresión regular para aceptar solo números
+                if (regex.test(value)) {
+                  setId(value); // Actualiza el estado solo si la entrada es un número
+                }
+              }}
+            />
+          </div>
+
+          <div className="p-inputgroup flex-1">
+            <span className="p-inputgroup-addon">
+              <i className="pi pi-user"></i>
+            </span>
+            <InputText
+              placeholder="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+
+          {/* Agregar input para la fecha */}
+          <div className="p-inputgroup flex-1">
+            <span className="p-inputgroup-addon">
+              <i className="pi pi-calendar"></i>
+            </span>
+            <Calendar
+              value={date}
+              onChange={(e) => setDate(e.value)}
+              placeholder="Fecha"
+              showIcon
+            />
+          </div>
+        </div>
+      </Dialog>
+    </div>
+  );
+};
+
+export default ModalEvent;
--- src/components/TableEvents.jsx
+++ src/components/TableEvents.jsx
@@ -1,31 +1,65 @@
 import React, { useState, useEffect } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-// import { Button } from "primereact/button";
-// import ModalDiscipline from "./ModalDiscipline";
+import ModalEvent from "./ModalEvent";
+import { Card } from "primereact/card";
+import NavBar from "./NavBar";
+import { InputText } from "primereact/inputtext";
 
 const TableEvents = () => {
   const [events, setEvents] = useState([]);
-  const [results, setResults] = useState([]);
+  const [eventSearch, setEventSearch] = useState("");
+  const [searchEvent, setSearchEvent] = useState(false);
+  const [originalEvents, setOriginalEvents] = useState([]); // Mantén una copia de respaldo de los eventos originales
 
   useEffect(() => {
-    loadEvents();
-  }, [events]);
+    if (searchEvent) {
+      searchEvents();
+    } else {
+      loadEvents();
+    }
+  }, [searchEvent]);
 
   const loadEvents = async () => {
     try {
       await fetch("http://localhost:4000/event")
-        .then((respuesta) => respuesta.json())
+        .then((response) => response.json())
         .then((resultado) => {
           resultado.data.forEach((event) => {
             event.date = new Date(parseInt(event.date)).toLocaleDateString();
           });
-          setEvents(respuesta.data);
+          setEvents(resultado.data);
+          setOriginalEvents(resultado.data); // Guarda una copia de los eventos originales
         });
     } catch (error) {
       console.error("Error al cargar eventos:", error);
     }
   };
+
+  const searchEvents = () => {
+    // Filtrar los eventos originales según el valor de búsqueda
+    const filteredEvents = originalEvents.filter((event) => {
+      // Verificar si el nombre del evento incluye las letras de eventSearch
+      return event.name.toLowerCase().includes(eventSearch.toLowerCase());
+    });
+
+    // Actualizar el estado de los eventos filtrados
+    setEvents(filteredEvents);
+  };
+
+  const search = (
+    <div style={{ display: "flex" }}>
+      <InputText
+        style={{ width: "100%" }}
+        onChange={(e) => {
+          const value = e.target.value;
+          setEventSearch(value);
+          setSearchEvent(value.trim() !== "");
+        }}
+      />
+      <ModalEvent />
+    </div>
+  );
 
   const header = (
     <div className="flex flex-wrap align-items-center justify-content-between">
@@ -36,31 +70,24 @@ const TableEvents = () => {
   const footer = `En total hay ${events ? events.length : 0} eventos.`;
 
   return (
-    <div className="card">
-      <DataTable
-        value={events}
-        header={header}
-        footer={footer}
-        tableStyle={{ minWidth: "60rem" }}
-      >
-        <Column field="id" header="ID"></Column>
-        <Column field="name" header="Nombre"></Column>
-        <Column field="date" header="Fecha"></Column>
-        {/* <Column
-        header="Resultados"
-        body={(rowData) => (
-          <ul>
-            {rowData.results.map((result, index) => (
-              <li key={index}>{result}</li>
-            ))}
-          </ul>
-        )}
-      ></Column> */}
-        {/* <Column
-        header="Editar"
-        body={(rowData) => <ModalDiscipline rowData={rowData} />}
-      ></Column> */}
-      </DataTable>
+    <div>
+      <NavBar />
+      <Card title={search} style={{ margin: "15px" }}>
+        <DataTable
+          value={events}
+          header={header}
+          footer={footer}
+          tableStyle={{ minWidth: "60rem" }}
+        >
+          <Column field="id" header="ID"></Column>
+          <Column field="name" header="Nombre"></Column>
+          <Column field="date" header="Fecha"></Column>
+          <Column
+            header="Editar"
+            body={(rowData) => <ModalEvent rowData={rowData} />}
+          ></Column>
+        </DataTable>
+      </Card>
     </div>
   );
 };
