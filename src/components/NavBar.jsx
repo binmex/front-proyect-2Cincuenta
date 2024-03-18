4c2e94fcc0cf7b5b9a3ab19dfb734ce70befce25
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2024-03-17 21:57:39 -0500

    Arreglo pagina principal

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,39 +1,45 @@
-import React from 'react'; 
-import { Menubar } from 'primereact/menubar';
+import React from "react";
+import { Menubar } from "primereact/menubar";
 
 const NavBar = () => {
-    const items = [
-        {
-            key: "disciplina",
-            label: 'Disciplina',
-            icon: 'pi pi-sitemap',
-            url: "/disciplina"
-        },
-        {
-            key:"eventos",
-            label: 'Eventos',
-            icon: 'pi pi-calendar',
-            url: "/eventos"
-        },
-        {
-            key: "afiliados",
-            label: 'Afiliados',
-            icon: 'pi pi-user-plus',
-            url: "/afiliados"
-        },
-        {
-            key: "resultados",
-            label: 'Resultados',
-            icon: 'pi pi-megaphone',
-            url: "/resultados"
-        }
-    ];
+  const items = [
+    {
+      key: "home",
+      label: "Inicio",
+      icon: "pi pi-home",
+      url: "/",
+    },
+    {
+      key: "disciplina",
+      label: "Disciplina",
+      icon: "pi pi-sitemap",
+      url: "/disciplina",
+    },
+    {
+      key: "eventos",
+      label: "Eventos",
+      icon: "pi pi-calendar",
+      url: "/eventos",
+    },
+    {
+      key: "afiliados",
+      label: "Afiliados",
+      icon: "pi pi-user-plus",
+      url: "/afiliados",
+    },
+    {
+      key: "resultados",
+      label: "Resultados",
+      icon: "pi pi-megaphone",
+      url: "/resultados",
+    },
+  ];
 
-    return (
-        <div className="card">
-            <Menubar model={items.map(item => ({...item, id: item.key}))} />
-        </div>
-    )
-}
+  return (
+    <div className="card">
+      <Menubar model={items.map((item) => ({ ...item, id: item.key }))} />
+    </div>
+  );
+};
 
 export default NavBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,44 @@
 import React from "react";
 import NavBar from "../components/NavBar";
-
+import backgroundImage from "../assets/img-bg-01.jpg";
+import { Card } from "primereact/card";
 
 const Home = () => {
-  return (<div>
-    <NavBar />
-    </div>);
+  const header = (
+    <img
+      alt="Card"
+      src={backgroundImage}
+      onError={(e) =>
+        (e.target.src =
+          "https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png")
+      }
+      style={{ width: "100%", height: "200px", objectFit: "cover" }}
+    />
+  );
+  return (
+    <>
+      <NavBar />
+      <div className="p-d-flex p-jc-center p-ai-center p-mt-5">
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Card
+            header={header}
+            className="p-mb-3"
+            style={{ width: "80%", marginTop: "20px", textAlign: "center" }}
+          >
+            <div className="p-d-flex p-flex-column p-jc-center p-ai-center">
+              <h1>Club Deportivo</h1>
+              <p>
+                Bienvenido a nuestra aplicación para gestionar la información
+                sobre los afiliados del club deportivo, las disciplinas que
+                ofrece, los eventos celebrados y los resultados obtenidos en
+                dichos eventos.
+              </p>
+            </div>
+          </Card>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
